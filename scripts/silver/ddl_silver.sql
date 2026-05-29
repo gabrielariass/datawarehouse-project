4e46eaed2a3ca7fd21695f3e6ceb94c346a4dbe8
Gabriel Arias <gabrielariaschinchilla@gmail.com>
2026-05-29 14:19:22 -0600

    Update ddl_silver.sql
    Updated silver.crm_prod_info

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -56,12 +56,13 @@ IF OBJECT_ID('silver.crm_prod_info','U') IS NOT NULL
 	DROP TABLE silver.crm_prod_info;
 CREATE TABLE silver.crm_prod_info (
 	prd_id			INT,
+	cat_id			NVARCHAR(50),
 	prd_key			NVARCHAR(50),
 	prd_nm			NVARCHAR(50),
 	prd_cost		INT,
 	prd_line		NVARCHAR(50),
-	prd_start_dt	DATETIME,
-	prd_end_dt		DATETIME,
+	prd_start_dt	DATE,
+	prd_end_dt		DATE,
 	dwh_create_date		DATETIME2 DEFAULT GETDATE()
 )
 
